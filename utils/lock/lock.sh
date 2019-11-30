f4be8627189d44f44c59f7fbb6d934509c2c4b94
i.polihronov <i.polihronov@ocado.com>
2019-11-30 11:46:47 +0200

    png directories fixed

--- utils/lock/lock.sh
+++ utils/lock/lock.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-ICON=$(shuf -n1 -e /home/ivo/lockConfig/*.png)
-ICONTEMP=/home/ivo/lockConfig/icon_temp.png
+ICON=$(shuf -n1 -e ~/.config/i3/utils/lock/Ar*.png)
+ICONTEMP= ~/.config/i3/utils/lock/icon_temp.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $ICON -resize 300x300 $ICONTEMP
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICONTEMP -gravity center -composite -matte $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 rm ICONTEMP
 #mutes the sound
 #pactl set-sink-mute @DEFAULT_SINK@ 1
 i3lock -u -i $TMPBG
+
